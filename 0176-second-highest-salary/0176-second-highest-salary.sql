b40ab1a0f39a7707036f7bdc9f64742a5307ba6b
Ankit Shrivastava <ankitrajsrivastava06@gmail.com>
2026-07-14 22:59:46 +0530

    Time: 380 ms (54.66%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,10 +1,8 @@
 /* Write your T-SQL query statement below */
--- select E1.salary as SecondHighestSalary 
---     from Employee E1
---     join Employee E2 on 
---     E1.salary = E2.salary where
---     E1.salary > E2.salary
-
-select max(salary) as SecondHighestSalary
-    from Employee 
-    where salary < (select max(salary) from Employee);
+SELECT MAX(salary) AS 
+    SecondHighestSalary
+        FROM Employee 
+        WHERE salary < (
+                SELECT MAX(salary) 
+                    FROM Employee
+                    );
